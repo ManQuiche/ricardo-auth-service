313efb7e12cdfc3405665f530cd991e63011c83e
Matéo RABOTIN <mateo@rabotin.net>
2022-03-14 18:21:28 +0100

    feat: switched login from username to email

--- internal/driven/db/cockroachdb/auth_repository.go
+++ internal/driven/db/cockroachdb/auth_repository.go
@@ -20,7 +20,7 @@ func NewAuthenticationRepository(client *gorm.DB) auth.AuthenticationRepository 
 
 func (r authenticationRepository) Exists(ctx context.Context, email, password string) (*entities.User, error) {
 	var user *entities.User
-	r.client.Where("username = ? and password = ?", email, password).First(&user)
+	r.client.Where("email = ? and password = ?", email, password).First(&user)
 
 	if user == nil {
 		return nil, errors.New("cannot find user")
