580cef360582e3c609f7c67c483ad08a75f0297e
Matéo RABOTIN <mateo@rabotin.net>
2022-06-27 22:32:02 +0200

    doc: added some explanation about RegisterNotifier

--- internal/core/ports/auth/register_notifier.go
+++ internal/core/ports/auth/register_notifier.go
@@ -2,6 +2,8 @@ package auth
 
 import "ricardo/auth-service/internal/core/entities"
 
+// RegisterNotifier Interface designed as a template for event
+// publishing function on user registration
 type RegisterNotifier interface {
 	Notify(user entities.User) error
 }
