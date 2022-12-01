443d67054e61bf60d65223a66560250d60ec64e0
rabotin <mateo.rabotin@dejamobile.com>
2022-12-01 20:10:55 +0100

    wip: wip

--- boot/firebase.go
+++ boot/firebase.go
@@ -5,7 +5,7 @@ import (
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
 	"gitlab.com/ricardo134/auth-service/pkg/errors"
-	option2 "google.golang.org/api/option"
+	"google.golang.org/api/option"
 )
 
 var (
@@ -15,8 +15,8 @@ var (
 
 func InitFirebaseApp() {
 	var err error
-	option := option2.WithCredentialsFile("boot/ricardo-9b5d5-firebase-adminsdk-udnxf-2e4b3b051f.json")
-	firebaseApp, err = firebase.NewApp(context.Background(), nil, option)
+	opt := option.WithCredentialsFile("boot/ricardo-9b5d5-firebase-adminsdk-udnxf-2e4b3b051f.json")
+	firebaseApp, err = firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		errors.CannotInitFirebaseApp()
 	}
